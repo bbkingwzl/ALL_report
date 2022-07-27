56528c0cc817846a657bdda6823662146f283625
bbkingwzl <105708747+bbkingwzl@users.noreply.github.com>
2022-07-27 23:05:12 +0800

    Update main.py

--- SM3_rho_method-main/main.py
+++ SM3_rho_method-main/main.py
@@ -3,15 +3,15 @@ import random
 
 set1 = set()
 flag =1
-
+i = str(random.randint(0, 2 ** 512))
 while(flag):
-    i = str(random.randint(0,2**256))
-    i1 = mySM3.SM3(i,0)
-    i2 = i1[0:16]
+    i1 = mySM3.SM3(i,1)
+    i2 = i1[0:8]
     if(i2 in set1):
         flag = 0
         print("True")
         print(i2)
     else:
         set1.add(i2)
+    i = i1
 print("end")
